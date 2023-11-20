8b3c6f05c71279d15a433df56559e3793f808321
JonWise82 <133918862+JonWise82@users.noreply.github.com>
2023-11-20 22:02:19 +0000

    Grid cells setup

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 //Model - application logic
 
+    
 
 
+//View - manipulate DOM
+function createGrid (gridSizeInput, minContainerDim) {    
+    let sketchContainer = document.getElementById("sketch-container");
+    const cellWidth = minContainerDim / gridSizeInput;
+    const cellWidthProperty = `${cellWidth}px`;
+    
+    document.documentElement.style.setProperty('--grid-cell-width', cellWidthProperty);
+    document.documentElement.style.setProperty('--grid-cell-height', cellWidthProperty);
 
-//View - updates the view
+    for (let i = 0; i < gridSizeInput ** 2; i++) {
+        let gridCell = document.createElement("div");
+        gridCell.classList.add('grid-cell');
+        sketchContainer.appendChild(gridCell);
+    }
+}
 
-
-
+function retrieveSketchContainerDimensions () {
+    let sketchContainer = document.getElementById("sketch-container");
+    const sketchContainerWidth = sketchContainer.clientWidth;
+    const sketchContainerHeight = sketchContainer.clientHeight;
+    return Math.min(sketchContainerWidth, sketchContainerHeight);
+}
 
 //Controller - listens to events
+const gridButton = document.getElementById('create-grid');
+
+gridButton.addEventListener('click', () => {
+    const gridSizeInput = +document.getElementById('input-grids').value;
+    createGrid(gridSizeInput, retrieveSketchContainerDimensions());
+})
