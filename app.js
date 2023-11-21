0948f7c5db0025016db4e42a49f43873efeb54cf
JonWise82 <133918862+JonWise82@users.noreply.github.com>
2023-11-21 23:01:21 +0000

    Change colour onclick hold

--- app.js
+++ app.js
@@ -4,32 +4,44 @@
 
 
 //View - manipulate DOM
-function createGrid (gridSizeInput, minContainerDim) {    
+function createGrid (gridSizeInput, containerDimensions) {    
     let sketchContainer = document.getElementById("sketch-container");
-    const cellWidth = minContainerDim / gridSizeInput;
+    const cellWidth = containerDimensions[0] / gridSizeInput;
+    const noRows = Math.floor(containerDimensions[1] / cellWidth);
     const cellWidthProperty = `${cellWidth}px`;
     
     document.documentElement.style.setProperty('--grid-cell-width', cellWidthProperty);
     document.documentElement.style.setProperty('--grid-cell-height', cellWidthProperty);
 
-    for (let i = 0; i < gridSizeInput ** 2; i++) {
+    for (let i = 0; i < gridSizeInput * noRows; i++) {
         let gridCell = document.createElement("div");
-        gridCell.classList.add('grid-cell');
+        gridCell.classList.add(`grid-cell-${i+1}`);
         sketchContainer.appendChild(gridCell);
     }
 }
 
 function retrieveSketchContainerDimensions () {
     let sketchContainer = document.getElementById("sketch-container");
-    const sketchContainerWidth = sketchContainer.clientWidth;
-    const sketchContainerHeight = sketchContainer.clientHeight;
-    return Math.min(sketchContainerWidth, sketchContainerHeight);
+    const style = window.getComputedStyle(sketchContainer);
+    
+    const totalWidth = parseFloat(style.width);
+    const totalHeight = parseFloat(style.height);    
+    const borderTop = parseFloat(style.borderTopWidth);
+    const borderBottom = parseFloat(style.borderBottomWidth);
+    const borderLeft = parseFloat(style.borderLeftWidth);
+    const borderRight = parseFloat(style.borderRightWidth);
+    
+    const sketchContainerWidth = totalWidth - borderLeft - borderRight;
+    const sketchContainerHeight = totalHeight - borderTop - borderBottom;
+    return [sketchContainerWidth, sketchContainerHeight];
 }
 
 //Controller - listens to events
 const gridButton = document.getElementById('create-grid');
 
 gridButton.addEventListener('click', () => {
+    let sketchContainer = document.getElementById("sketch-container");
+    sketchContainer.innerHTML = "";
     const gridSizeInput = +document.getElementById('input-grids').value;
     createGrid(gridSizeInput, retrieveSketchContainerDimensions());
 })
