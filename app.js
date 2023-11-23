9e603bcf9b4239f88dd32b7dd2a6967ffa85d5e0
JonWise82 <133918862+JonWise82@users.noreply.github.com>
2023-11-23 05:50:40 +0000

    First working version

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 //View - manipulate DOM
 function createGrid (gridSizeInput, containerDimensions) {    
-    let sketchContainer = document.getElementById("sketch-container");
     const cellWidth = containerDimensions[0] / gridSizeInput;
     const noRows = Math.floor(containerDimensions[1] / cellWidth);
     const cellWidthProperty = `${cellWidth}px`;
@@ -15,13 +14,17 @@ function createGrid (gridSizeInput, containerDimensions) {
 
     for (let i = 0; i < gridSizeInput * noRows; i++) {
         let gridCell = document.createElement("div");
-        gridCell.classList.add(`grid-cell-${i+1}`);
+        // Unique for all grid cells
+        gridCell.id = `grid-cell-${i+1}`;
+        // Class type for all grid cells
+        gridCell.classList.add(`grid-cell`);
+        // Add an event listener to each grid cell
+        gridCell.addEventListener('click', handleClick);
         sketchContainer.appendChild(gridCell);
     }
 }
 
 function retrieveSketchContainerDimensions () {
-    let sketchContainer = document.getElementById("sketch-container");
     const style = window.getComputedStyle(sketchContainer);
     
     const totalWidth = parseFloat(style.width);
@@ -38,10 +41,15 @@ function retrieveSketchContainerDimensions () {
 
 //Controller - listens to events
 const gridButton = document.getElementById('create-grid');
+const sketchContainer = document.getElementById("sketch-container");
 
-gridButton.addEventListener('click', () => {
-    let sketchContainer = document.getElementById("sketch-container");
+//Generate grid button
+gridButton.addEventListener('click', () => {    
     sketchContainer.innerHTML = "";
     const gridSizeInput = +document.getElementById('input-grids').value;
     createGrid(gridSizeInput, retrieveSketchContainerDimensions());
 })
+   
+function handleClick(event) {
+    event.target.style.backgroundColor = 'red';
+}
